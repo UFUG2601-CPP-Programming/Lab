295ace908f57d631efe831dd97494c4b6b9c8c7b
Jzjerry <jzjerry@foxmail.com>
2023-09-01 16:11:21 +0800

    change structure of lab00 and add Makefile

--- lab00/lab00.cpp
+++ lab00/lab00.cpp
@@ -1,7 +1,8 @@
 //1.    Enter your header file here
 
 //2.    Define your main function here. 
-
+int main(){
 //2.1.  Print "Hello World" here.
 
 //2.2.  Remeber to return a value of main function
+}
