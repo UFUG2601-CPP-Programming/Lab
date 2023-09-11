265634350a17001a474a1c461a237443ce9c0231
Jzjerry <jzjerry@foxmail.com>
2023-09-11 15:41:57 +0800

    lab01 template and makefile

---
+++ lab01/lab01.cpp
@@ -0,0 +1,26 @@
+// Include the header files you need
+
+// We already defined the main function for you
+// But don't forget to define it when you are coding on your own
+int main(){
+
+    // Task 1 and 2: 
+    // Integer Numbers, Floating Numbers, Character, String
+    
+    // Declare variables with different data types
+
+    // Assign values to the variables correctly
+
+    // Output variables and check the values
+
+    // Task 3:
+    // Limits
+
+    // Use std::numeric_limits to get the limits of data types
+
+    // Output limits
+
+    // Don't forget to return 0 in the end
+}
+
+
