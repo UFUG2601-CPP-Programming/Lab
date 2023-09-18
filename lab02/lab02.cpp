361526fbf07b4fb90a4c167aad61a6f228642bf1
jzjerry <jzjerry@foxmail.com>
2023-09-18 23:52:20 +0800

    update lab02.cpp and Makefile

---
+++ lab02/lab02.cpp
@@ -0,0 +1,22 @@
+//1.    Enter your header file here
+
+//2.    Define your main function here. 
+int main(){
+
+    // 2.1 Declare two integer varibles here
+    int a, b;
+    // 2.2 Declare a integer varible for result
+    int result;
+    // 2.3 Calculate with different operators here
+    // For example: result = a + b;
+    // Try as many operators as you can to observe different results
+    
+    result = /* Fill your expression here*/;
+
+    // (Optional) Try to declare varibles with different types.
+    // And try to apply the operators above again.
+    
+    // 2.4 Output the result
+
+    // Remeber to return a value of main function
+}
